6e4a97c6e60c8a09829c692b961fe77c6818125b
hanseulhee <3021062@gmail.com>
2021-03-23 21:31:37 +0900

    boj_1000

---
+++ README.md
@@ -0,0 +1,11 @@
+## Problem Solving with Java
+
+---
+
+#### 21.03.23
+
+- BOJ 1000
+    
+    간단한 입력 후 더하기 문제, scanner 사용
+
+  
--- src/boj_1000.java
+++ src/boj_1000.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class boj_1000 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int two = sc.nextInt();
+
+        int answer = first + two;
+
+        System.out.print(answer);
+
+        sc.close(); // scanner을 종료한다.
+
+    }
+
 }

cc8d3eaed4cb6ff150df52074badc5b4be6de856
hanseulhee <3021062@gmail.com>
2021-03-30 15:05:29 +0900

    Upload Readme

--- src/boj_4101.java
+++ src/boj_4101.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 public class boj_4101 {
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
 
-        int first = sc.nextInt();
-        int second = sc.nextInt();
+        while(true) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
 
-        if(first > second){
-            System.out.print("Yes");
-        }else if(first < second){
-            System.out.print("No");
-        }else if(first == second){
-            System.out.print("No");
-        }else if(first == 0 && second == 0){
+            if (first == 0 && second == 0) {
+                break;
+            }else if (first < second) {
+                System.out.println("No");
+            } else if (first == second) {
+                System.out.println("No");
+            } else if (first > second) {
+                System.out.println("Yes");
+            }
         }
 
+        sc.close();
     }
 }
